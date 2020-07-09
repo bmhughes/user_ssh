2f4ed695c1c6d2805c45f71c472136faf472522e
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2020-07-09 22:15:45 +0100

    Update LICENSE, metadata and kitchen config

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,12 @@
 name 'user_ssh'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures user_ssh'
+maintainer 'Ben Hughes'
+maintainer_email 'bmhughes@bmhughes.co.uk'
+license 'Apache-2.0'
+description 'Configures user SSH keys and options'
 version '0.1.0'
-chef_version '>= 15.0'
+chef_version '>= 14.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/user_ssh/issues'
+issues_url 'https://github.com/bmhughes/user_ssh/issues'
+source_url 'https://github.com/bmhughes/user_ssh'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/user_ssh'
+%w(centos fedora redhat debian ubuntu).each { |os| supports os }
